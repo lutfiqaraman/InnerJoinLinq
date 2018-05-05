de8431172b590bcfc5d5947b16e9c089a53ced2e
lutfiqaraman <lutfiqaraman@gmail.com>
2018-05-05 22:30:25 +0300

    Done

---
+++ InnerJoinLinq/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerJoinLinq
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
--- InnerJoinLinq/Program.cs
+++ InnerJoinLinq/Program.cs
@@ -10,6 +10,42 @@ namespace InnerJoinLinq
     {
         static void Main(string[] args)
         {
+            List<Product> ProductList = new List<Product>
+            {
+                new Product {ProductID = 1, ProductName = "Mobile"},
+                new Product {ProductID = 2, ProductName = "Charger"},
+                new Product {ProductID = 3, ProductName = "Pocket"},
+                new Product {ProductID = 4, ProductName = "Tablet"},
+                new Product {ProductID = 5, ProductName = "LapTop"},
+            };
+
+            List<Order> ProductOrders = new List<Order>
+            {
+                new Order {OrderID = 1, ProductID = 1, PaymentMethod = "Cheque"},
+                new Order {OrderID = 2, ProductID = 5, PaymentMethod = "Credit"},
+                new Order {OrderID = 3, ProductID = 1, PaymentMethod = "Cash"},
+                new Order {OrderID = 4, ProductID = 3, PaymentMethod = "Cheque"},
+                new Order {OrderID = 5, ProductID = 3, PaymentMethod = "Cheque"},
+                new Order {OrderID = 6, ProductID = 4, PaymentMethod = "Cash"}
+            };
+
+            var OrderforProducts =
+                from product in ProductList
+                join order in ProductOrders
+                on product.ProductID equals order.ProductID
+                select new
+                {
+                    product.ProductID,
+                    Name = product.ProductName,
+                    order.PaymentMethod
+                };
+
+            foreach (var item in OrderforProducts)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
